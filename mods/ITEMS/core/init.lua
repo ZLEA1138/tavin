f53c0ab6ce9231aca7aa09dd99838293c0715e2a
ZLEA1138 <leaz@my.erau.edu>
2024-08-04 22:31:10 -0400

    pre1

---
+++ mods/ITEMS/core/init.lua
@@ -0,0 +1,9 @@
+local mod_name = minetest.get_current_modname()
+local mod_path = minetest.get_modpath(mod_name)
+
+tavin_core = {}
+
+-- makes nodes
+dofile(mod_path .. DIR_DELIM .. "nodes_base.lua")
+dofile(mod_path .. DIR_DELIM .. "nodes_liquid.lua")
+dofile(mod_path .. DIR_DELIM .. "abms.lua")
---
+++ mods/MAPGEN/mapgen/init.lua
@@ -0,0 +1,18 @@
+
+-- these tell minetest what nodes to generate in the world
+minetest.register_alias("mapgen_stone", "sb_core:stone")
+minetest.register_alias("mapgen_water_source", "sb_core:water_source")
+minetest.register_alias("mapgen_river_water_source", "sb_core:water_source")
+
+mapgen = {}
+
+local mod_name = minetest.get_current_modname()
+local mod_path = minetest.get_modpath(mod_name)
+
+function mapgen.get_schematic_path(name)
+    return (mod_path .. DIR_DELIM .. "schematics" .. DIR_DELIM .. name .. ".mts")
+end
+
+-- biomes
+dofile(mod_path .. DIR_DELIM .. "biomes" .. DIR_DELIM .. "grasslands.lua")
+dofile(mod_path .. DIR_DELIM .. "biomes" .. DIR_DELIM .. "ocean.lua")
